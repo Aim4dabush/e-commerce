2359ee7d99874286983123121bd12485bbd72c7c
aim4dabush <edarasato@gmail.com>
2023-01-01 22:52:16 -0800

    design login page, global button, and added print width to prettier config

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,32 @@
 import styles from "./Login.module.scss";
 
+//components
+import Button from "../../components/GlobalComponents/Button/Button";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <form className={styles.form}>
+        <h1 className={styles.title}>Login</h1>
+        <div className={styles.inputControl}>
+          <label htmlFor="email">Email</label>
+          <input className={styles.input} type="email" id="email" />
+        </div>
+        <div className={styles.inputControl}>
+          <label htmlFor="password">Password</label>
+          <input className={styles.input} type="password" id="password" />
+        </div>
+        <Button>Login</Button>
+        <p className={styles.text}>
+          Not a member?{" "}
+          <Link className={styles.signUp} to={"/sign-up"}>
+            Sign up here
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
