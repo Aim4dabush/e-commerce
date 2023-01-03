9be697db0318d78bb6ce9888698c1b14f70321dd
aim4dabush <edarasato@gmail.com>
2023-01-02 18:37:01 -0800

    changed brand to a link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 //components
+import { NavLink } from "react-router-dom";
 import PageLinks from "./PageLinks/PageLinks";
 
 //styles
@@ -7,7 +8,9 @@ import styles from "./NavBar.module.scss";
 const NavBar = () => {
   return (
     <nav className={styles.container}>
-      <h1 className={styles.brand}>What's The Deal</h1>
+      <NavLink className={styles.brand} to={"/"}>
+        <h1>What's The Deal</h1>
+      </NavLink>
       <PageLinks />
     </nav>
   );
