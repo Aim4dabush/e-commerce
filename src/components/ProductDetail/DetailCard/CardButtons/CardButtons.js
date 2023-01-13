6583d9af74048d8f65529f238d40a4413b9695a6
aim4dabush <edarasato@gmail.com>
2023-01-12 22:11:25 -0800

    added navigation to cart and wish list buttons

--- src/components/ProductDetail/DetailCard/CardButtons/CardButtons.js
+++ src/components/ProductDetail/DetailCard/CardButtons/CardButtons.js
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+//components
+import SecondaryButton from "../../../GlobalComponents/SecondaryButton/SecondaryButton";
+
+//styles
 import styles from "./CardButtons.module.scss";
 
 const CardButtons = () => {
+  const navigate = useNavigate();
+
+  const navigateToCart = () => {
+    navigate("/cart");
+  };
+
+  const navigateToWishList = () => {
+    navigate("/wish-list");
+  };
+
   return (
     <div className={styles.btnContainer}>
-      <button className={styles.btn}>Add To Cart</button>
-      <button className={styles.btn}>Add To Wish List</button>
+      <SecondaryButton onClickHandler={navigateToCart}>Add To Cart</SecondaryButton>
+      <SecondaryButton onClickHandler={navigateToWishList}>Add To Wish List</SecondaryButton>
     </div>
   );
 };
